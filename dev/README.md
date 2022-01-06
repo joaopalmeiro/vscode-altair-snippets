34b9bd822d1554e52f63a11b739812ad18586b0b
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-06 11:40:24 +0000

    Bootstrap extract snippet script

--- dev/README.md
+++ dev/README.md
@@ -2,4 +2,5 @@
 
 - `pipenv install --python 3.8`
 - `pipenv run jupyter lab`
+- `pipenv run python extract_snippet.py`
 - `pipenv run fmt`
---
+++ dev/extract_snippet.py
@@ -0,0 +1,15 @@
+import orjson
+
+NOTEBOOK: str = "dev.ipynb"
+ID: str = ""
+
+if __name__ == "__main__":
+    with open(NOTEBOOK) as f:
+        nb: str = f.read()
+
+    # https://github.com/ijl/orjson#deserialize
+    nbo = orjson.loads(nb)
+
+    snippet = nbo["cells"]
+
+    print(snippet)
